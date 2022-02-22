91b51d44704029696bf7a19c4ff62855523e6cd4
Rafael Assis Fontes <fael_assis@hotmail.com>
2022-02-22 19:37:23 -0300

    meu primeiro commit

---
+++ js/index.js
@@ -0,0 +1,29 @@
+//let predio = ["terreo","primeiroAndar","segundoAndar"];
+
+//predio.push("quartoAndar") adiciona item no final da lista
+//predio.unshift("subsolo") adiciona item no começo da lista
+//predio.shift() RETIRA um item do começo da lista
+//predio.pop() RETIRA um item do final da lista
+
+/*for (let contador = 0; contador < 3; contador++){
+    console.log(contador);
+}*/
+
+/*let estados = ["rio", "sao paulo", "bahia"]
+
+for(let i = 0; i < estados.length; i++){
+    console.log(estados[i])
+}*/
+
+//objetos em java
+/*let minhaXicara = {
+    cor: marrom,
+    peso: 30
+}*/
+
+let meuPrimeiroPedido = {
+    nome: "Hotdot",
+    peso: 300
+}
+
+console.log(meuPrimeiroPedido.peso + "g")
